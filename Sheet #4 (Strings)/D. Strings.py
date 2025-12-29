5618236f5250b87b22f0ed0d01492e9dfeab08a6
moussa755 <mossm808@gmail.com>
2025-12-30 01:22:39 +0200

    problm solving

---
+++ Sheet #4 (Strings)/D. Strings.py
@@ -0,0 +1,5 @@
+a=input()
+b=input()
+print(len(a),len(b))
+print(a+b)
+print(b[0]+a[1:],a[0]+b[1:])
---
+++ Sheet #4 (Strings)/E. Count.py
@@ -0,0 +1,7 @@
+s = input()
+x=0
+for i in range(len(s)):
+    y=s[i]
+    y=int(y)
+    x=x+y
+print(x)
