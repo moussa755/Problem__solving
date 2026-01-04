0ad1d6a60cc7527a38b2729a0e6f258405b10979
moussa755 <mossam808@gmail.com>
2026-01-04 04:24:19 +0200

    problem solving

--- Contest # 2/C. Finding Minimums.py
+++ Contest # 2/C. Finding Minimums.py
@@ -1,4 +1,8 @@
-n=int(input())
-a=int(input())
-#for  i in range (a):
-    
+a,b=map(int,input().split())
+for  i in range (a):
+    mo = list(map(int,input().split()))
+    print (mo)
+    for i in range (b):
+        num=mo[i]
+        print(num)
+        
